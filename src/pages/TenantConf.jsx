8595ecb8ba3dc9808dbc9fba486664cfcf3e53cc
timothy <timothyjohan11@gmail.com>
2023-11-22 00:23:11 +0700

    setTimeout
    ehe

--- src/pages/TenantConf.jsx
+++ src/pages/TenantConf.jsx
@@ -16,14 +16,14 @@ export default function TenantConf(){
     setLoad(false);
     setTimeout(() => {
       navigate("/")
-    }, 500);
+    }, 100);
     
   }
 
   return(
     <>
       <div className="pt-28 min-h-screen">
-        <div className={`${btnClick ? 'scale-0 transition-all duration-300' : ''} ${load ? 'scale-100 transition-all duration-300' : 'scale-0'} flex items-center justify-center mt-20 bg-neutral-800 w-4/6 mx-auto p-10 text-neutral-200 rounded-md`}>
+        <div className={`${btnClick ? '-translate-x-full transition-all duration-300' : ''} ${load ? '-translate-x-0 transition-all duration-300' : 'translate-x-full'} flex items-center justify-center mt-20 bg-neutral-800 w-4/6 mx-auto p-10 text-neutral-200 rounded-md`}>
           <div className="text-center">
             <h1 className="text-2xl mb-10">Syarat dan ketentuan</h1>
             <p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, ex a nihil beatae reiciendis doloribus cupiditate explicabo quasi deserunt quisquam impedit non corrupti modi ipsum facere sed architecto? Laudantium, facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi iure placeat accusantium sint repellendus, explicabo ducimus maiores culpa vero. Expedita a ut commodi vitae eveniet nobis esse iure, ducimus facilis. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem, eos delectus incidunt earum cumque nostrum voluptatibus necessitatibus alias quod, nobis aliquam? Illo aliquam eos ex nesciunt neque esse harum fugiat?</p>
