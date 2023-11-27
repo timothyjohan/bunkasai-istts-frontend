71912a27d77575568a4fc85c388768aee243a278
lthreen <timothyjohan11@gmail.com>
2023-11-27 14:17:07 +0700

    Tenant form ui

--- src/pages/TenantForm.jsx
+++ src/pages/TenantForm.jsx
@@ -26,11 +26,25 @@ export default function TenantForm(){
   return(
     <>
       <div className="pt-28 min-h-screen">
-        <div className={`${disagreeClick ? '-translate-x-full transition-all duration-300' : ''} ${load ? '-translate-x-0 transition-all duration-300' : 'translate-x-full'} flex items-center justify-center mt-20 bg-neutral-800 w-4/6 mx-auto p-10 text-neutral-200 rounded-md`}>
-          <div className="text-center">
-            <h1 className="text-2xl mb-10">Form pendaftaran tenant</h1>
-            <input type="text" placeholder="Nama Tenant" className="w-full p-2" />
-            
+        <div className={`${disagreeClick ? '-translate-x-full transition-all duration-300' : ''} ${load ? '-translate-x-0 transition-all duration-300' : 'translate-x-full'} flex items-center justify-center mt-20 bg-neutral-800/80 w-2/6 mx-auto p-10 text-neutral-200 rounded-xl`}>
+          <div className="">
+            <form>
+              <h1 className="text-2xl mb-10 text-center">Form pendaftaran tenant</h1>
+              <label htmlFor="nama_tenant" className="m-2">Nama tenant</label>
+              <input type="text" id="nama_tenant" placeholder="Nama Tenant" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
+              <br /> <br />
+              <label htmlFor="nm_cp" className="m-2">Nama contact person</label>
+              <input type="text" id="nm_cp" placeholder="Nama contact person" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
+              <br /> <br />
+              <label htmlFor="notel" className="m-2">Nomor telp</label>
+              <input type="phone" id="notel" placeholder="Nomor telp" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
+              <br /><br />
+              <label htmlFor="alamat" className="m-2">Alamat</label>
+              <input type="phone" id="alamat" placeholder="contoh: Jl. Ngagel Jaya Tengah No.73-77" className="w-full p-2 px-4 bg-neutral-700 rounded-xl mb-10" />
+              <button type="submit" className="bg-neutral-700 w-full py-2 rounded-xl hover:font-bold transition-all hover:scale-110 hover:text-violet-500 hover:bg-green-400">Submit</button>
+              
+
+            </form>
           </div>
           
         </div>
