21d2ac4c35a254dc9feab330eb91b36ea3050307
lthreen <timothyjohan11@gmail.com>
2023-11-27 16:00:11 +0700

    minor details

--- src/pages/TenantForm.jsx
+++ src/pages/TenantForm.jsx
@@ -26,7 +26,7 @@ export default function TenantForm(){
   return(
     <>
       <div className="pt-28 min-h-screen">
-        <div className={`${disagreeClick ? '-translate-x-full transition-all duration-300' : ''} ${load ? '-translate-x-0 transition-all duration-300' : 'translate-x-full'} flex items-center justify-center mt-20 bg-neutral-800/80 w-2/6 mx-auto p-10 text-neutral-200 rounded-xl`}>
+        <div className={`${disagreeClick ? '-translate-x-full transition-all duration-300' : ''} ${load ? '-translate-x-0 transition-all duration-300' : 'translate-x-[100vw]'} flex items-center justify-center mt-20 bg-neutral-800/80 w-2/6 mx-auto p-10 text-neutral-200 rounded-xl`}>
           <div className="">
             <form>
               <h1 className="text-2xl mb-10 text-center">Form pendaftaran tenant</h1>
@@ -37,7 +37,7 @@ export default function TenantForm(){
               <input type="text" id="nm_cp" placeholder="Nama contact person" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
               <br /> <br />
               <label htmlFor="notel" className="m-2">Nomor telp</label>
-              <input type="phone" id="notel" placeholder="Nomor telp" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
+              <input type="phone" id="notel" placeholder="contoh: 0812XXXXX" className="w-full p-2 px-4 bg-neutral-700 rounded-xl" />
               <br /><br />
               <label htmlFor="alamat" className="m-2">Alamat</label>
               <input type="phone" id="alamat" placeholder="contoh: Jl. Ngagel Jaya Tengah No.73-77" className="w-full p-2 px-4 bg-neutral-700 rounded-xl mb-10" />
