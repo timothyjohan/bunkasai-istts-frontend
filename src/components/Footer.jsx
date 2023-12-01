e3c286fc1c229bcdf5759f3b71337f7b36817ec2
lthreen <timothyjohan11@gmail.com>
2023-12-01 09:52:42 +0700

    Footer fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(){
     return(
         <>
-            <div className="bg-neutral-900 h-56 text-neutral-300">
+            <div className="bg-neutral-900 min-h-fit pb-5 text-neutral-300">
                 <footer className="grid grid-cols-2">
                     <div className="w-11/12 mx-auto border-r border-solid border-neutral-500 mt-10">
                         <h1 className="text-xl ">Contact Person</h1>
@@ -13,12 +13,12 @@ export default function Footer(){
                         <h1 className="font-bold">2023 &copy; BUNKASAI ISTTS</h1>
                     </div>
                     <div className="w-11/12 mx-auto border-neutral-500 mt-10 grid grid-cols-2">
-                        <a href="https://instagram.com/bunkasaiistts" target="_blank">
-                            <div className="flex items-center">
-                                <img src="/instagram-logo.png" alt="" className="w-5 h-5 brightness-75" />
-                                <p className="mx-2">@bunkasaiistts</p>
+                            <div className="">
+                                <a href="https://instagram.com/bunkasaiistts" className="flex items-center" target="_blank">
+                                    <img src="/instagram-logo.png" alt="" className="w-5 h-5 brightness-75" />
+                                    <p className="mx-2">@bunkasaiistts</p>
+                                </a>
                             </div>
-                        </a>
                         <div className="text-right">
                             <p className="mb-5">Jl. Ngagel Jaya Tengah No.73-77 · 0821-2290-7788</p>
                             <p className="mb-5">Surabaya, Jawa Timur, Indonesia</p>
