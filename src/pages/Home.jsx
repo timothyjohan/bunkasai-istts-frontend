481bde590e0669c2f455ea71020e6712dde46976
lthreen <timothyjohan11@gmail.com>
2023-12-01 12:02:13 +0700

    Scroll reveal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Fade } from "react-reveal";
 
 export default function Home(){
   const [load, setLoad] = useState(false)
@@ -19,26 +20,51 @@ export default function Home(){
       <div className="min-h-screen mt-44 w-5/6 mx-auto text-neutral-200">
         <div className="grid grid-cols-2">
 
-          <div className="">
-            <img className="opacity-90 max-h-[85%] " src="/clariss-pose-1.png" alt="" />
-          </div>
+          <Fade>
+            <div>
+              <img className="opacity-90 max-h-[85%] " src="/clariss-pose-1.png" alt="" />
+            </div>
+          </Fade>
 
-          <div className="text-right mt-20">
-            <h1 className="text-4xl font-semibold">MEET CLARISS</h1>
-            <br />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus corrupti soluta quas quia possimus obcaecati reiciendis debitis veniam reprehenderit adipisci molestias dicta, quisquam, impedit, ex dolorem perspiciatis sed ipsum. lorem</p>
-          </div>
+          <h1>
+              <div className="text-right h-2/6 mt-20">
+                <Fade bottom>
+                  <h1 className="text-4xl font-semibold">MEET CLARISS</h1>
+
+                </Fade>
+                <br />
+                
+                <p>
+                  <Fade bottom cascade>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus corrupti soluta quas quia possimus obcaecati reiciendis debitis veniam reprehenderit adipisci molestias dicta, quisquam, impedit, ex dolorem perspiciatis sed ipsum. lorem</p>
+                    
+
+                  </Fade>
+                </p>
+              </div>
+
+
+          </h1>
 
 
           <div className="text-left mt-20">
-            <h1 className="text-4xl font-semibold">MEET CLUE</h1>
+            <Fade>
+              <h1 className="text-4xl font-semibold">MEET CLUE</h1>
+
+            </Fade>
             <br />
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus corrupti soluta quas quia possimus obcaecati reiciendis debitis veniam reprehenderit adipisci molestias dicta, quisquam, impedit, ex dolorem perspiciatis sed ipsum. lorem</p>
+            <Fade bottom cascade>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi doloribus corrupti soluta quas quia possimus obcaecati reiciendis debitis veniam reprehenderit adipisci molestias dicta, quisquam, impedit, ex dolorem perspiciatis sed ipsum. lorem</p>
+
+            </Fade>
           </div>
 
-          <div className="">
-            <img className="opacity-90 max-h-[85%] " src="/clue-pose-2.png" alt="" />
-          </div>
+          <Fade>
+            <div className="">
+              <img className="opacity-90 max-h-[85%] " src="/clue-pose-2.png" alt="" />
+            </div>
+
+          </Fade>
 
         </div>
       </div>
