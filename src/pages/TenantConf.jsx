fdda42891fffb77fbc86a0cda9ba0bb44d632e33
lthreen <timothyjohan11@gmail.com>
2024-02-21 17:01:42 +0700

    Fix tenant white screen on right

--- src/pages/TenantConf.jsx
+++ src/pages/TenantConf.jsx
@@ -66,11 +66,11 @@ export default function TenantConf() {
                     } ${
                         page.page > page.previous
                             ? load
-                                ? "-translate-x-0 transition-all duration-300"
-                                : "translate-x-full"
+                                ? "xl:-translate-x-0 xl:transition-all duration-300"
+                                : "xl:translate-x-full"
                             : load
-                            ? "translate-x-0 transition-all duration-300"
-                            : "-translate-x-full"
+                            ? "xl:translate-x-0 xl:transition-all duration-300"
+                            : "xl:-translate-x-full"
                     } flex items-center justify-center mt-20 bg-neutral-800/80 w-4/6 mx-auto p-10 text-neutral-200 rounded-md`}
                 >
                     <div className="text-center">
