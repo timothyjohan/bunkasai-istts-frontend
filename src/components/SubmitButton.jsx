68eec987662754fd34d8c8ed740571d3c3eaadfd
NeutralDM <go.greg.a.gosal1234@gmail.com>
2025-05-30 14:49:12 +0700

    recolor buttons

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,27 +1,27 @@
 export default function SubmitButton({ btnClick, ShowErrors }) {
-    return (
-        <>
-            {btnClick ? (
-                <button
-                    type="submit"
-                    disabled
-                    className="w-full py-2 rounded-xl font-bold transition-all text-violet-500 bg-green-400"
-                >
-                    <img
-                        src="loading.png"
-                        className="h-6 mx-auto transition-all animate-spin"
-                        alt="Loading"
-                    />
-                </button>
-            ) : (
-                <button
-                    type="submit"
-                    className="bg-neutral-700 w-full py-2 rounded-xl hover:font-bold transition-all hover:scale-110 hover:text-violet-500 hover:bg-green-400"
-                    onClick={ShowErrors}
-                >
-                    Submit
-                </button>
-            )}
-        </>
-    );
+  return (
+    <>
+      {btnClick ? (
+        <button
+          type="submit"
+          disabled
+          className="w-full py-2 rounded-xl font-bold transition-all text-violet-500 bg-green-400"
+        >
+          <img
+            src="loading.png"
+            className="h-6 mx-auto transition-all animate-spin"
+            alt="Loading"
+          />
+        </button>
+      ) : (
+        <button
+          type="submit"
+          className="bg-neutral-700 w-full py-2 rounded-xl hover:font-bold transition-all hover:scale-110 hover:bg-[#5071BE] hover:text-white"
+          onClick={ShowErrors}
+        >
+          Submit
+        </button>
+      )}
+    </>
+  );
 }
